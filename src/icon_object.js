a6174772529c380f8c3321a5bcacb08a4c0187fa
Amenadiel <amenadiel@gmail.com>
2018-11-12 18:46:40 -0300

    Tag v 2.12.6 if google maps is present, return size and point as google maps classes

--- src/icon_object.js
+++ src/icon_object.js
@@ -5,6 +5,24 @@ function IconObject(url, fillColor, markerOpts) {
 	this.fillColor = fillColor;
 	this.markerOpts = markerOpts;
 	Object.assign(this, markerOpts);
+	if (window && window.google && window.google.maps) {
+		this.origin = new google.maps.Point(
+			this.markerOpts.origin.x,
+			this.markerOpts.origin.y
+		);
+		this.anchor = new google.maps.Point(
+			this.markerOpts.anchor.x,
+			this.markerOpts.anchor.y
+		);
+		this.size = new google.maps.Size(
+			this.markerOpts.size.width,
+			this.markerOpts.size.height
+		);
+		this.scaledSize = new google.maps.Size(
+			this.markerOpts.scaledSize.width,
+			this.markerOpts.size.height
+		);
+	}
 	return this;
 }
 
